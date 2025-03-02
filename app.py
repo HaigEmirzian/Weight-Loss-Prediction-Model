79ff8e90ceadb8dae0d45214702c475fcbabf0da
Haig Emirzian <haig.emirzian@gmail.com>
2025-03-02 14:05:55 -0500

    imported logic into flask app

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask, request, jsonify
 import RNN_model
 import requests
+import torch
+import torch.nn as nn
+from torch.optim import Adam
 
 app = Flask(__name__)
 
@@ -17,12 +20,34 @@ def prediction():
         if not allowed_file(file.filename):
             return jsonify({"error": "File type not allowed"})
         try:
-        # Load weights
-        # Transform data into tensor
-        # Train on data
-        # Predict future weight
-        # Return as json
-            return jsonify({'prediction': 0})
+            # Configure device to CPU or CUDA
+            device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+            # Load data and transform into tensors
+            filename = input("Enter the file path: ")
+            no_of_days = int(input("Enter the number of days to predict: "))
+            X_train, X_test, y_train, y_test, df, data_min, data_max = RNN_model.load_and_preprocess_data(filename, window_size=30)
+            train_loader, test_loader = RNN_model.prepare_data_loaders(X_train, X_test, y_train, y_test)
+
+            # Initialize model, loss function, and optimizer
+            model = RNN_model.Weight_Model().to(device)
+            loss_fn = nn.MSELoss()
+            optimizer = Adam(model.parameters(), lr=0.001)
+
+            # Train on data
+            RNN_model.train_model(model, train_loader, loss_fn, optimizer, num_epochs=1000)
+
+            # Evaluate model
+            RNN_model.evaluate_model(model, test_loader, loss_fn, data_min, data_max)
+
+            # Retrieve historical weights and predict future weight
+            historical_weights = df['Weight'].values
+            weight_tensor = torch.tensor(historical_weights).to(torch.float32)
+
+            new_data = weight_tensor[-no_of_days:]
+            predicted_weight = RNN_model.predict_weight(model, new_data, data_min, data_max)
+
+            return jsonify({'prediction': predicted_weight})
         except:
             return jsonify({"error": 'error during prediction'})
 
